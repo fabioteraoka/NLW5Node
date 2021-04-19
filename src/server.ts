7004835c540625ed1bab8bb774a2d1dd87523acc
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2021-04-19 13:19:20 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app = express();
+
+app.listen(3333,()=> console.log("server is running on port 3333")
+)
+
+// * GET = busca
+// * POST = Criação
+// * PUT = Alteração
+// * DELETE =  Apagar
+// * PATCH = Alterar uma informação especifica
+
+app.get("/", (req,res)=>{
+    // return res.send("Ola NLW 05")
+    return res.json({
+        message: "Ola NLW 05 JSON",
+    });
+})
+
+app.post("/",(req,res)=>{
+    return res.json({
+        message:"Usuario salvo com sucesso!"
+    });
+})
