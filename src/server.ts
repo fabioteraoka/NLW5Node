4e2a568a94b2bf95f6234d274f533978b26ccfc8
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2021-04-20 16:15:02 -0300

    Criado as Entidades

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from "express";
 
+import "./database";
+
 const app = express();
 
 app.listen(3333,()=> console.log("server is running on port 3333")
 )
-
 // * GET = busca
 // * POST = Criação 
 // * PUT = Alteração
